e0bc9ebbd70ff1d06c00c46e058ae768298fdcab
matej <matej.djerdji@gmail.com>
2023-10-11 19:46:51 +0200

    Exercise 8.19 - books by genre part1

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,19 +1,48 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = () => {
   const result = useQuery(ALL_BOOKS)
+  const [genre, setGenre] = useState(null)
+
+  const genreSelector = (genre) => {
+    return () => setGenre(genre)
+  }
 
   if (result.loading) {
     return <div>loading books...</div>
   }
 
   const books = result.data.allBooks
+  const displayBooks = genre
+    ? books.filter(b => b.genres.includes(genre))
+    : books
+
+  const genres = new Set()
+  books.forEach(b => {
+    b.genres.forEach(g => genres.add(g))
+  })
+
+  const selectedStyle = {
+    fontWeight: 'bold',
+    color: 'darkgreen'
+  }
 
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        Filter by genre:
+        <button style={genre === null ? selectedStyle : {}} onClick={genreSelector(null)}>All</button>
+        {
+          [...genres].map(g => {
+            return g === genre
+              ? <button key={g} style={selectedStyle} onClick={genreSelector(g)}>{g}</button>
+              : <button key={g} onClick={genreSelector(g)}>{g}</button>
+          })
+        }
+      </div>
       <table>
         <tbody>
           <tr>
@@ -21,13 +50,15 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {
+            displayBooks.map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )
+            )}
         </tbody>
       </table>
     </div>
